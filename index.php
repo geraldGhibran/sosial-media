6624f1852b8907f55c873611fb78ae4faf143f6f
Gerald Ghibran <gerald.gunasha@gmail.com>
2021-01-22 10:54:25 +0700

    modified:   index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-8">
-                        <h1>Selamat datang di Pesbuk</h1>
+                        <h1>Selamat datang di Pesbuk, Berikut hostname anda => <?php echo gethostname(); ?></h1>
                         <p>Bergabunglah bersama jutaan orang lainnya...</p>
                     </div>
                     <div class="col-md-4">
