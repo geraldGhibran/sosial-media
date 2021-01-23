76ccc8e85c2b63210f397754e88361d2dac86278
Gerald Ghibran <gerald.gunasha@gmail.com>
2021-01-23 21:15:13 +0700

    modified:   register.php

---
+++ config2.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_host = "10.0.2.203";
+$db_user = "root";
+$db_pass = "masukaja";
+$db_name = "dbsosmed";
+// asdda
+//test
+//Yhhhh ini commecnt
+
+try {    
+    //create PDO connection 
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+} catch(PDOException $e) {
+    //show error
+    die("Terjadi masalah: " . $e->getMessage());
+}
